ffd00a4e172b0dd5757a4989d299d9ad5659d0a1
Teo <teodor.rd27@gmail.com>
2017-06-04 19:26:42 +0100

    make steady progress, create controller and maxObject.js

--- controllers.js
+++ controllers.js
@@ -1,15 +1,20 @@
 'use strict';
 otcApp.controller("buttonsController", ['$scope', 'maxFactory', function($scope, maxFactory){
+  var object = maxFactory.object;
   $scope.first = function() {
-    var object = maxFactory.object;
+
     var newNumber = parseInt(object.red) + 1;
-    console.log(newNumber);
-    maxFactory.update(newNumber);
+    maxFactory.update(newNumber, 'red');
+    console.log(object.red);
   };
   $scope.second = function() {
-    console.log("GOODBYE");
+    var newNumber = parseInt(object.yellow) + 1;
+    maxFactory.update(newNumber, 'yellow');
+    console.log(object.yellow);
   };
   $scope.third =function() {
-    console.log("HOWYA");
+    var newNumber = parseInt(object.green) + 1;
+    maxFactory.update(newNumber, 'green');
+    console.log(object.green);
   };
 }]);
--- maxObject.js
+++ maxObject.js
@@ -6,8 +6,9 @@ otcApp.factory('maxFactory', function(){
     yellow: '0',
     green: '0'
   };
-  maxFac.update = function(newNumber){
-    maxFac.object.red = newNumber;
+  maxFac.update = function(newNumber, index){
+    maxFac.object[index] = newNumber;
+    console.log(maxFac.object);
   };
   return maxFac;
 });
